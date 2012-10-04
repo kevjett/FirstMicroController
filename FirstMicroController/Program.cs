fee8f29bd7267b29c843dc89b6fdb069b1380398
Kevin Jett <kevjett@gmail.com>
2012-10-03 21:54:48 -0500

    project start

---
+++ FirstMicroController/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+
+namespace FirstMicroController
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            OutputPort LED;
+            LED = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, true);
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+    }
+}
